c7602bc6574c09d7725b58994fba6c05756c7443
anoush <dakeon@outlook.fr>
2023-09-16 19:20:32 +0200

    readme and comments in the code

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 little script to convert a `text` file with song titles on each line to a folder with the corresponding `mp3s`
 
+make sure to complete the last line (the last song name) in the text file by pressing enter afterwards and having a blank line at the end. otherwise the last song wont be downloaded
+
 ## usage
 
 `src/txt2mp3.sh PATH`
--- src/txt2mp3.sh
+++ src/txt2mp3.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
+# if no argument is given the script stops
 if [ $# -eq 0 ]
 	then 
 		echo Usage txt2mp3.sh PATH, where PATH is the txt file path
 
 else
+    # saving the user input (the filepath)
     input="$1"
 
+    # making sure the script is running inside the project root directory
     cd "$(dirname "$(dirname "$0")")"
 
     # creating "mp3s" directory if not already existing
     mkdir -p mp3s
     cd mp3s
 
+    # reading the text file line by line
     while read -r line
     do
+    # running yt-dlp to do the download part for us
     yt-dlp --extract-audio --add-metadata --embed-thumbnail --audio-quality 0 --default-search "ytsearch" --audio-format mp3 "$line"
     done < "../$input"
 
